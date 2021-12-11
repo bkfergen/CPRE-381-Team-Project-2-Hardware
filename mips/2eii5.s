da02bd23da0ad8502a8301ac04fc82df3b714289
bkfergen <bkfergen@iastate.edu>
2021-12-10 22:06:15 -0600

    tests for 2 e ii

---
+++ mips/2eii5.s
@@ -0,0 +1,27 @@
+# Test 0 - SW (store word)
+#
+# The objective of this test assembly file will be to check if an arbritrary data
+# value stored in any of the argument, temporary, and saved registers, can be put
+# into the starting address allocation of the data segment (0x10010000).
+# My justification for including only these registers is that I think that these are
+# likely going to involved in holding data that needs to potentially be sent out to data
+# segment; therefore, the other remaining registers are not included in this test. The
+# functionality of those other registers will be checked in other tests.
+
+.data
+.text
+.globl main
+main:
+    	# Start Test
+    	addi 	$t1, $0, 1
+	addi	$t2, $0, 2
+	sub	$t0, $t2, $t1
+	add	$t0, $t0, $t1
+    	bne 	$t0, $t1, exit
+	addi	$t3, $0, 5
+
+exit:
+    	# Exit program
+		halt
+    	li $v0, 10
+    	syscall
